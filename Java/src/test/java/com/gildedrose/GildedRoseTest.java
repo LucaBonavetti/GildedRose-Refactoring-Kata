80b25bee4c850dafa1f0495c908f81e7a857f159
LucaBonavetti <161732253+LucaBonavetti@users.noreply.github.com>
2025-05-28 17:22:07 +0200

    added test for unchanged sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -67,4 +67,13 @@ class GildedRoseTest {
         assertEquals(1, items[0].sellIn);
         assertEquals(50, items[0].quality);
     }
+
+    @Test
+    void testSulfurasNeverChanges() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
 }
