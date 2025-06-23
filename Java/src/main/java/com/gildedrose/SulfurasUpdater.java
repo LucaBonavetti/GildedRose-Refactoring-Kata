4717e161ec1bde615a08528c6e82c9a753f76810
LucaBonavetti <161732253+LucaBonavetti@users.noreply.github.com>
2025-06-23 18:33:07 +0200

    added SulfurasUpdater

---
+++ Java/src/main/java/com/gildedrose/SulfurasUpdater.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class SulfurasUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        // Legendary item does not change
+    }
+}
