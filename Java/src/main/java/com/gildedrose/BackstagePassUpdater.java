a66787a724af8f98eae31cbd34e4f267e28659d0
LucaBonavetti <161732253+LucaBonavetti@users.noreply.github.com>
2025-06-23 18:33:35 +0200

    added BackstagePassUpdater

---
+++ Java/src/main/java/com/gildedrose/BackstagePassUpdater.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+public class BackstagePassUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality = Math.min(item.quality + 3, 50);
+        } else if (item.sellIn < 10) {
+            item.quality = Math.min(item.quality + 2, 50);
+        } else {
+            if (item.quality < 50) {
+                item.quality++;
+            }
+        }
+    }
+}
